30df8bbb64fa6c9197e0dc75f725eb710d2eff5f
John Smith <rintran720@gmail.com>
2024-01-21 09:45:50 +0700

    chore: add husky, prettier, eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import { Button } from "../components/ui/button";
-import { ModeToggle } from "../components/mode-toggle";
+import { Button } from '../components/ui/button';
+import { ModeToggle } from '../components/mode-toggle';
 
 export default function Home() {
   return (
